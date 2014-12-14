83a8873a889b39f03e4f4831b395b6affdf85697
ssrp <ssrpsathira@gmail.com>
2014-12-14 13:31:34 +0530

    create random data genrating script.

--- index.php
+++ index.php
@@ -1,22 +1,35 @@
 <?php
 
-$data = array('metadata' => array('service' => 'noise', 'mode' => 'upload'), 'rawdata' => array('latitude' => '7', 'longitude' => '81', 'noise_level' => '45.67', 'date_time' => ''));
-$data_string = json_encode($data);
+for ($i = 0; $i < 100000; $i++) {
+    $randomLatitude = floatVal(rand(6, 9) . '.' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9));
+    $randomLongitude = rand(79, 81);
+    if ($randomLongitude == 79) {
+        $randomLongitude = floatVal($randomLongitude . '.' . rand(7, 9) . '' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9));
+    } elseif ($randomLongitude == 81) {
+        $randomLongitude = floatVal($randomLongitude . '.' . rand(0, 5) . '' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9));
+    } else {
+        $randomLongitude = floatVal($randomLongitude . '.' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9) . '' . rand(0, 9));
+    }
+    $randomSoundLevel = floatVal(rand(30, 130) . '.' . rand(0, 9) . '' . rand(0, 9));
+    $randomTimeStamp = rand(1398643200, 1418542140);
 
-$url = 'http://156.56.93.34/CDME/request.php';
+    $data = array('metadata' => array('service' => 'noise', 'mode' => 'upload'), 'rawdata' => array('latitude' => $randomLatitude, 'longitude' => $randomLongitude, 'noise_level' => $randomSoundLevel, 'date_time' => $randomTimeStamp));
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json',
-    'Content-Length: ' . strlen($data_string))
-);
-$result = curl_exec($ch);
-curl_close($ch);
+    $data_string = json_encode($data);
 
-echo $result;
+    $url = 'http://156.56.93.34/CDME/request.php';
 
-//$json_result = json_decode($result, true);
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($data_string))
+    );
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    echo $result;
+}
 ?>
